f75caf12c0d1bb2cd86cb1723dd38ce51778cc21
Mukhammadaziz <rustamov977075073@gmail.com>
2024-03-05 15:16:33 +0500

    3-dars

---
+++ main.js
@@ -0,0 +1,36 @@
+// let son = 10;
+
+// let floor = Math.floor(son);
+// let ceil = Math.ceil(son);
+// console.log(son);
+
+
+
+// let natija = Math.trunc(Math.random()*100);
+// alert(natija);
+
+// let son = prompt('bitta son kiriting');
+
+// console.log(Math.trunc(Math.sqrt(son)));
+// console.log(Math.pow(son,2));
+// console.log(son * 2);
+// console.log(son);    
+
+// let son1 = prompt('Hozir ekranda random son chiqadi');
+// let son2 = Math.trunc(Math.random()*100);
+
+// alert('Random son: '+ a);
+// alert('Biz random sonni 2 ga kopaytiramiz');
+// alert(`Random son: ${son1} \n Bu sonni 2 ga kopaytirdik: ${son1 * 2} \n bu sonni `);
+
+let b = prompt('hozir ekranda random sonlar chiqadi')
+
+let a = Math.trunc(Math.random()*100)
+
+alert('random son:' +  a)
+
+alert('biz random sonni 2 ga kopaytiramiz')
+
+alert(`random son: ${a} \n bu soni 2ga kopaytirdik: ${a * 2} \n bu sonni 2ga boldi ${a /2} \n va bu sonni 3ga boldik ${a % 3}`)
+
+const abc = Math.trunc(Math.random()*100);
